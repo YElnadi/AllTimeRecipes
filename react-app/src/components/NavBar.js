5030add153ef739e924c7ed4cbeb0fabf680ebf4
Yasmine Elnadi <yasmine_adel@hotmail.com>
2023-01-25 20:45:22 -0800

    navigation bar

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,38 +1,46 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import LogoutButton from "./auth/LogoutButton";
+import { useSelector } from "react-redux";
+import "./NavBar.css";
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user);
+
   return (
     <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
+      {sessionUser ?( <i className="fa-solid fa-gear login-signup-btn" style={{fontSize:20}}></i> )
+      :(
+      <div className="login-signup-btn">
+        <div>
+          <NavLink to="/login" exact={true} activeClassName="active">
             Login
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+        </div>
+        <div>
+          <NavLink to="/sign-up" exact={true} activeClassName="active">
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
+        </div>
+      </div>
+      )}
+
+      <div>
+        <NavLink to="/" exact={true} activeClassName="active">
+          Home
+        </NavLink>
+      </div>
+
+      <div>
+        <NavLink to="/users" exact={true} activeClassName="active">
+          Users
+        </NavLink>
+      </div>
+      <div>
+        <LogoutButton />
+      </div>
     </nav>
   );
-}
+};
 
 export default NavBar;
